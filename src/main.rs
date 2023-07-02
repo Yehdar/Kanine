3cfa33830db57b04c77fd1d0cbde915af86c6214
Yehdar <p.radhey06@gmail.com>
2023-07-01 22:59:38 -0400

    feat: add cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("hollup... let him cook");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    
+    let flag = args[1].clone();
+    let threads = args[2].clone();
+    let ip = args[3].clone();
 }
