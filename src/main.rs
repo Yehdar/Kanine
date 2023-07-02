f3ae65a391520eaca14f809869573144210e1f34
Yehdar <p.radhey06@gmail.com>
2023-07-01 23:02:08 -0400

    refactor: organize cli arguments into struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::env;
 
+struct Arguments {
+    flag: String,
+    ip: Ip,
+    threads: u16,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
     
-    let flag = args[1].clone();
-    let threads = args[2].clone();
-    let ip = args[3].clone();
 }
