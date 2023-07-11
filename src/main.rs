3fb42c788ebd658d4aa3e8cbfe2ce78e07d2bc02
Yehdar <p.radhey06@gmail.com>
2023-07-11 08:00:07 -0400

    add multithreading by following thread documentation in std

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::net::IpAddr;
 use std::str::FromStr;
 use std::process;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
 
 struct Arguments {
     flag: String,
@@ -50,12 +52,22 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
     let arguments = Arguments::new(&args).unwrap_or_else();
-        |err| {
+    |err| {
         if err.contains("help"){
             process::exit(0);
         } else {
             eprintln!("{} problem parsing arguments: {}", program, err);
             process::exit(0);
         }
+    };
+
+    let num_threads = arguments.threads;
+    let (tx, rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, arguments.ipaddr, num_threads);
+        });
     }
 }
