47903fa82766f768f43e3bc5c453a0a255031c3a
Yehdar <p.radhey06@gmail.com>
2023-07-14 08:18:21 -0400

    refactor: handle dead code

--- src/main.rs
+++ src/main.rs
@@ -73,18 +73,18 @@ fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16){
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
-    let arguments = Arguments::new(&args).unwrap_or_else();
-    |err| {
+    let arguments = Arguments::new(&args).unwrap_or_else(|err| {
         if err.contains("help"){
             process::exit(0);
         } else {
             eprintln!("{} problem parsing arguments: {}", program, err);
             process::exit(0);
         }
-    };
+    });
 
     let num_threads = arguments.threads;
     let addr = arguments.ipaddr;
+    let _kisee_ko_pata_nahin_chalega = arguments.flag;
     let (tx, rx) = channel();
     for i in 0..num_threads {
         let tx = tx.clone();
